eb56bb3a40a576608c95547c121912f2c1112647
smrutiranjanbhuyan <smruti452004@gmai.com>
2024-02-03 23:16:37 +0530

    changed

--- REACT/my-app/src/Sources/Navbar.js
+++ REACT/my-app/src/Sources/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <><nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar</a>
+      <a className="navbar-brand" href="#">{props.headName}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -31,10 +31,11 @@ export default function Navbar() {
             <a className="nav-link disabled" aria-disabled="true">Disabled</a>
           </li>
         </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        
+        <div className="form-check form-switch">
+  <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault"></label>
+</div>
       </div>
     </div>
   </nav>
